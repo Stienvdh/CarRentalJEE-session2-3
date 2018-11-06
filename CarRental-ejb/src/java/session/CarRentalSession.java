fe0a60a212ded2e00bf2d2f7bfc7e82ed7d45807
Joke Denissen <joke.denissen@hotmail.com>
2018-11-06 12:25:33 +0100

    CarRentalSession EntityManager

--- CarRental-ejb/src/java/session/CarRentalSession.java
+++ CarRental-ejb/src/java/session/CarRentalSession.java
@@ -1,11 +1,15 @@
 package session;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import rental.CarRentalCompany;
 import rental.CarType;
 import rental.Quote;
 import rental.RentalStore;
@@ -15,36 +19,46 @@ import rental.ReservationException;
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
+    
+    @PersistenceContext
+    private EntityManager em;
 
     private String renter;
     private List<Quote> quotes = new LinkedList<Quote>();
 
     @Override
     public Set<String> getAllRentalCompanies() {
-        return new HashSet<String>(RentalStore.getRentals().keySet());
+        List<String> resultList = this.em.createNamedQuery("allRentalCompanyNames").getResultList();
+        if (resultList == null)
+            return new HashSet<String>();
+        return new HashSet<String>(resultList);
     }
     
     @Override
     public List<CarType> getAvailableCarTypes(Date start, Date end) {
+        List<CarRentalCompany> resultList = this.em.createNamedQuery("allRentalCompanies").getResultList();
         List<CarType> availableCarTypes = new LinkedList<CarType>();
-        for(String crc : getAllRentalCompanies()) {
-            for(CarType ct : RentalStore.getRentals().get(crc).getAvailableCarTypes(start, end)) {
+        if(resultList == null) {
+            resultList = new ArrayList<CarRentalCompany>();
+        }
+        for(CarRentalCompany crc : resultList) {
+            for (CarType ct : crc.getAvailableCarTypes(start, end)){
                 if(!availableCarTypes.contains(ct))
                     availableCarTypes.add(ct);
-            }
+            } 
         }
         return availableCarTypes;
     }
 
     @Override
     public Quote createQuote(String company, ReservationConstraints constraints) throws ReservationException {
-        try {
-            Quote out = RentalStore.getRental(company).createQuote(constraints, renter);
-            quotes.add(out);
-            return out;
-        } catch(Exception e) {
-            throw new ReservationException(e);
+        CarRentalCompany crc = this.em.find(CarRentalCompany.class, company);
+        if (crc == null) {
+            throw new ReservationException("Company " + company + " doesn't exist.");
         }
+        Quote out = crc.createQuote(constraints, renter);
+        quotes.add(out);
+        return out;
     }
 
     @Override
@@ -57,11 +71,14 @@ public class CarRentalSession implements CarRentalSessionRemote {
         List<Reservation> done = new LinkedList<Reservation>();
         try {
             for (Quote quote : quotes) {
-                done.add(RentalStore.getRental(quote.getRentalCompany()).confirmQuote(quote));
+                CarRentalCompany crc = this.em.find(CarRentalCompany.class, quote.getRentalCompany());
+                done.add(crc.confirmQuote(quote));
             }
         } catch (Exception e) {
-            for(Reservation r:done)
-                RentalStore.getRental(r.getRentalCompany()).cancelReservation(r);
+            for(Reservation r:done){
+                CarRentalCompany crcRollBack = this.em.find(CarRentalCompany.class, r.getRentalCompany());
+                crcRollBack.cancelReservation(r);                
+            }
             throw new ReservationException(e);
         }
         return done;
